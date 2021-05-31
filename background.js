865bcac5c844e2e49705386702d2f7d955880e1b
Phil Cohen <pcohen@snapchat.com>
2021-05-30 17:54:35 -0700

    fix

--- background.js
+++ background.js
@@ -1,19 +1,23 @@
 chrome.webNavigation['onDOMContentLoaded'].addListener(async (evt) => {
-  const data = {
-    url: evt.url,
-    ts: evt.timeStamp,
-    evt,
-  };
-  const tab = await chrome.tabs.get(evt.tabId);
-  data.tab = tab;
-  if (tab.openerTabId) {
-    const openerTab = await chrome.tabs.get(tab.openerTabId);
-    data.referrer = openerTab.url;
-    data.openerTab = openerTab;
+  try {
+    const data = {
+      url: evt.url,
+      ts: evt.timeStamp,
+      // evt,
+    };
+    const tab = await chrome.tabs.get(evt.tabId);
+    // data.tab = tab;
+    if (tab.openerTabId) {
+      const openerTab = await chrome.tabs.get(tab.openerTabId);
+      // data.referrer = openerTab.url;
+      // data.openerTab = openerTab;
+    }
+    const resp = await fetch("http://0.0.0.0:7445", {
+      method: 'POST',
+      mode: 'no-cors',
+      body: JSON.stringify(data),
+    });
+  } catch (e) {
+
   }
-  const resp = await fetch("http://0.0.0.0:7445", {
-    method: 'POST',
-    mode: 'no-cors',
-    body: JSON.stringify(data),
-  });
 });
