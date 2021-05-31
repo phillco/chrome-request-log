b5dfd6abb74e6bc3914c25479f5818301b9c4070
Noah Tye <hi@noahtye.com>
2021-05-30 17:36:03 -0700

    send req to daemon

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
-chrome.webNavigation['onDOMContentLoaded'].addListener(e => {
-
-
+chrome.webNavigation['onDOMContentLoaded'].addListener(async (evt) => {
+  const resp = await fetch("http://0.0.0.0:7445", {
+    mode: 'no-cors',
+    body: JSON.stringify({
+      url: evt.url,
+      ts: evt.timeStamp,
+    }),
+  });
 });
