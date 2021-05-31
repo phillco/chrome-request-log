2c63fe7e02a558282201b0a00be2510ef82820d7
Noah Tye <hi@noahtye.com>
2021-05-30 17:37:54 -0700

    POST to daemon

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.webNavigation['onDOMContentLoaded'].addListener(async (evt) => {
   const resp = await fetch("http://0.0.0.0:7445", {
+    method: 'POST',
     mode: 'no-cors',
     body: JSON.stringify({
       url: evt.url,
